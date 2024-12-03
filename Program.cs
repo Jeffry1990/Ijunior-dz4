626867c48eb343707bcf3b6550c6bb57ee2aebe7
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-03 20:14:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Артур";
+            string surname = "Володин";
+            string temporaryName;
+
+            Console.WriteLine("До замены:");
+            Console.WriteLine($"Имя - {name}");
+            Console.WriteLine($"Фамилия - {surname}");
+
+            temporaryName = name;
+            name = surname;
+            surname = temporaryName;
+
+            Console.WriteLine();
+            Console.WriteLine("После замены:");
+            Console.WriteLine($"Имя - {name}");
+            Console.WriteLine($"Фамилия - {surname}");
+        }
+    }
+}
